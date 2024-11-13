d22dd5f3335ba6889fb770e961c72da94495b4ab
Muzamil ALi <muzamil.6aug24webgpt@gmail.com>
2024-11-13 13:04:43 +0500

    ok

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -5,7 +5,7 @@ const router = express.Router();
 const {userRegister,login,registerStudent,studentsofLoginuser,studentUpdate,delteStudent} =require('../controllers/userController.js');
 
 const middleware = require('../middleware/auth.js');
-const generateOTP= require('../middleware/mailer.js')
+// const generateOTP= require('../middleware/mailer.js')
 // user registration 
 router.post('/userRegister',userRegister);
 
@@ -15,7 +15,7 @@ router.post('/login',login);
 
 router.post('/studentRegister',middleware,registerStudent);
 
-router.get('/studentsofLoginuser',middleware,generateOTP,studentsofLoginuser);
+router.get('/studentsofLoginuser',middleware,studentsofLoginuser);
 
 router.patch('/studentUpdate/:studentId',middleware,studentUpdate);
 
